dcd40bec3a3a26cf0765f8cd1c1d013fced8bebe
kurwella <novik.alexey.phone@gmail.com>
2024-09-21 20:42:24 +0200

    commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,19 @@
+package org.example;
+
+import org.example.repo.Person;
+import org.example.repo.PersonRepo;
+
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
+
+        PersonRepo pr = new PersonRepo();
+
+        Person person = new Person("Bob", 22);
+        pr.addPerson(person);
+
+        Person found = pr.findPersonById(1L);
+        System.out.println(found.getName());
+    }
+}
